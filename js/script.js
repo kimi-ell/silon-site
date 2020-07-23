f2507327781385a942839d86b6afb6be12b37d49
kimi-ell <60607789+kimi-ell@users.noreply.github.com>
2020-07-23 23:58:45 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 
     $(document).ready(function(){
     $('.header-bottom-icon-menu').click(function(event) {
-        $('.header-bottom-icon-menu,.header-nav').toggleClass('active');
+        $('.header-bottom-icon-menu,.header-nav-list').toggleClass('active');
         $('body').toggleClass('lock');
     });
     $('.header-nav-menu-list').click(function(event){
-        $('.header-bottom-icon-menu,.header-nav').removeClass('active');
+        $('.header-bottom-icon-menu,.header-nav-list').removeClass('active');
         $('body').removeClass('lock')
     });
 });
+// $(document).ready(function(){
+//     $('.header-bottom-icon-menu').on('click', function(e) {
+//         e.preventDefault();
+//         $('.header-nav-list').slideToggle();
+//     })
+// })
+
     function testWebP(callback) {
 
     var webP = new Image();
@@ -26,6 +33,41 @@ testWebP(function (support) {
         document.querySelector('body').classList.add('no-webp');
     }
 });
+    $(document).ready(function(){
+    $('.popular-bottom-row').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 870,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 639,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },  
+            
+        ]
+    });
+});
+
 
 
   
